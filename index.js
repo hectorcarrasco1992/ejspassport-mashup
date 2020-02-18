e997ceac22a3e8f0eba88c003ae75386fec1a196
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2020-02-17 23:23:30 -0500

    made movies page

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const app = express()
+const fetch = require('node-fetch')
+require('dotenv').config()
+const logger = require('morgan')
+const path = require('path')
+const port = process.env.PORT || 3000
+
+
+app.use(logger('dev'))
+
+
+
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'views'))
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'Public')))
+
+
+app.get('/movies',(req,res)=>{
+    
+    const url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=';
+    const urlEnd = '&language=en-US&page=1';
+    const apiKey = process.env.API_KEY
+    const img = 'https://image.tmdb.org/t/p/w500';
+    fetch(url+apiKey+urlEnd)
+        .then((movies) => movies.json())
+        .then((movies) => {
+            
+            
+            console.log(movies.results)
+            const all = movies.results 
+            res.render('movies', { all,img})
+        })
+        .catch((err) => console.log(err))
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`listening on ${port}`)
+})
